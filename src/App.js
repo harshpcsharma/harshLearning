e94104ef8f775da4f199fb85a18612372cf87776
harshpcsharma <harshpcsharma@gmail.com>
2024-01-05 19:13:38 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Harsh want's to learn React.
         </p>
         <a
           className="App-link"
